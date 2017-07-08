87c39970ff354fe7aff61a9a8b419fc9b4633010
Poff Poffenberger <poffdeluxe@gmail.com>
2017-07-08 11:03:18 -0500

    Initial commit, bro

---
+++ index.js
@@ -0,0 +1,30 @@
+// Load up our config
+require('dotenv').config()
+
+const gdax = require('./gdax');
+
+/**
+ * Responds to any HTTP request that can provide a "message" field in the body.
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
+exports.helloWorld = function helloWorld (req, res) {
+  if (req.body.message === undefined) {
+    // This is an error case, as "message" is required
+    res.status(400).send('No message defined!');
+  } else {
+    // Everything is ok
+    console.log(req.body.message);
+    res.status(200).send(req.body.message).end();
+  }
+};
+
+/*
+Deploy with:
+gcloud beta functions deploy hello --entry-point helloWorld --stage-bucket cbt3000 --trigger-http
+
+call with:
+
+curl -X POST https://us-central1-cryptbrotrader3000.cloudfunctions.net/hello -H "Content-Type:application/json" --data '{"message":"Keyboard Cat"}'
+*/
